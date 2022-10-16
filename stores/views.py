6a10ee2166ec7c7be958754e2d4c02a789e31928
BLoushi52 <k.albloushii@hotmail.com>
2022-10-16 20:58:17 +0300

    created form

--- stores/views.py
+++ stores/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from stores import models
-
+from .forms import StoreItemForm
 
 def get_store_items(request: HttpRequest) -> HttpResponse:
     store_items: list[models.StoreItem] = list(models.StoreItem.objects.all())
@@ -10,3 +10,10 @@ def get_store_items(request: HttpRequest) -> HttpResponse:
         "store_items": store_items,
     }
     return render(request, "store_item_list.html", context)
+
+def create_store_item(request):
+    form = StoreItemForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'create_store_item.html', context)
